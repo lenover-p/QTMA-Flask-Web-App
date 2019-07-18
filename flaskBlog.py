da991a0f04dd5ab020df2b75a3bb1e705dbd4ba6
lenover-p <16pl16@queensu.ca>
2019-07-18 18:05:28 -0400

    Added HTML templates

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,16 +1,32 @@
-from flask import Flask, escape, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Patrick Lenover',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'April 20, 2019'
+    },
+    {
+        'author': 'John Johnson',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'April 23, 2019'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-	return "<h1>About Page</h1>"
+	return render_template('about.html', title='About')
 
 if __name__ == '__main__':
 	app.run(debug=True)
