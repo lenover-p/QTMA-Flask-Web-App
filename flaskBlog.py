b64858ed49a6b900fcd6d0cc3573a7073cc54e6d
lenover-p <16PL16@queensu.ca>
2019-07-18 21:00:26 -0400

    Added register and log in pages

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '7bfcb5a3aad9b4ab3290c7370b334ec9'
 
 posts = [
     {
@@ -17,7 +19,6 @@ posts = [
     }
 ]
 
-
 @app.route('/')
 @app.route('/home')
 def home():
@@ -27,6 +28,25 @@ def home():
 @app.route('/about')
 def about():
 	return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Log in failed. Try again', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
